7ff434d9c187efbfe92e72f76fd361ee6d198b02
Franchesca Barias <franchesca.barias7@gmail.com>
2022-05-25 21:08:02 +0100

    Variables created that takes input

---
+++ main.py
@@ -0,0 +1,10 @@
+# Produce a programme that displays the prime numbers between 1 and 100
+# Prime number only has two factors, 1 and itself
+
+# Variables created that takes input
+# Converted string to integer
+lower = int(input("Enter the minimum number: "))
+upper = int(input("Enter the maximum number: "))
+
+
+
